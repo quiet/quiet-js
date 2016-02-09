58be5f608c692722e90cac20ad5be4b2437d9abc
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-02-09 15:53:17 -0800

    only fetch profilename on dom ready

--- javascripts/transmit_text.js
+++ javascripts/transmit_text.js
@@ -1,14 +1,16 @@
 var TextTransmitter = (function() {
     Transmitter.setProfilesPath("javascripts/profiles.json");
-    var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
-    var transmit = Transmitter.transmitter(profilename);
+    document.addEventListener("DOMContentLoaded", function() {
+        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
+        var transmit = Transmitter.transmitter(profilename);
 
-    document.querySelector('[data-quiet-send-button]').addEventListener('click',
-        function(e) {
-            var payload = document.querySelector('[data-quiet-text-input]').value;
-            if (payload === "") {
-                return
-            }
-            transmit(payload);
-    }, false);
+        document.querySelector('[data-quiet-send-button]').addEventListener('click',
+            function(e) {
+                var payload = document.querySelector('[data-quiet-text-input]').value;
+                if (payload === "") {
+                    return
+                }
+                transmit(payload);
+        }, false);
+    };
 })();
