ca8d3ccee16098f47cc2da2453dddd0f1050f5b1
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-02-09 15:55:22 -0800

    check for readyCallback undefined
    also prefix emscripten items with Module.

--- javascripts/transmit.js
+++ javascripts/transmit.js
@@ -9,7 +9,7 @@ var Transmitter = (function() {
     function start() {
         audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
         console.log(audio_ctx.sampleRate);
-        if (readyCallback !== null) {
+        if (readyCallback !== undefined) {
             readyCallback();
         }
     };
@@ -21,27 +21,27 @@ var Transmitter = (function() {
     };
 
     function newTransmitter(profilename) {
-        var c_profiles = intArrayFromString(profiles);
-        var c_profilename = intArrayFromString(profilename);
-        var opt = ccall('get_encoder_profile_str', 'pointer', ['array', 'array'], [c_profiles, c_profilename]);
-        ccall('encoder_opt_set_sample_rate', 'number', ['pointer', 'number'], [opt, audio_ctx.sampleRate]);
+        var c_profiles = Module.intArrayFromString(profiles);
+        var c_profilename = Module.intArrayFromString(profilename);
+        var opt = Module.ccall('get_encoder_profile_str', 'pointer', ['array', 'array'], [c_profiles, c_profilename]);
+        Module.ccall('encoder_opt_set_sample_rate', 'number', ['pointer', 'number'], [opt, audio_ctx.sampleRate]);
         c
-        var encoder = ccall('create_encoder', 'pointer', ['pointer'], [opt]);
+        var encoder = Module.ccall('create_encoder', 'pointer', ['pointer'], [opt]);
 
         return function(payloadStr, done) {
-            var payload = allocate(intArrayFromString(payloadStr), 'i8', ALLOC_NORMAL);
-            ccall('encoder_set_payload', 'number', ['pointer', 'pointer', 'number'], [encoder, payload, payload.length]);
+            var payload = allocate(Module.intArrayFromString(payloadStr), 'i8', ALLOC_NORMAL);
+            Module.ccall('encoder_set_payload', 'number', ['pointer', 'pointer', 'number'], [encoder, payload, payload.length]);
 
             var sample_len = bufferSize;
-            var samples = ccall('malloc', 'pointer', ['number'], [4 * sample_len]);
-            var sample_view = HEAPF32.subarray((samples/4), (samples/4) + sample_len);
+            var samples = Module.ccall('malloc', 'pointer', ['number'], [4 * sample_len]);
+            var sample_view = Module.HEAPF32.subarray((samples/4), (samples/4) + sample_len);
 
             var script_processor = (audio_ctx.createScriptProcessor || audio_ctx.createJavaScriptNode);
             var transmitter = script_processor.call(audio_ctx, sample_len, 1, 2);
             transmitter.onaudioprocess = function(e) {
                 var output_offset = 0;
                 var output_l = e.outputBuffer.getChannelData(0);
-                var written = ccall('encode', 'number', ['pointer', 'pointer', 'number'], [encoder, samples, sample_len]);
+                var written = Module.ccall('encode', 'number', ['pointer', 'pointer', 'number'], [encoder, samples, sample_len]);
                 output_l.set(sample_view);
                 if (written < sample_len) {
                     for (var i = written; i < sample_len; i++) {
