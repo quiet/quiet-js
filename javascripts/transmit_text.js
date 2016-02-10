ffebb1ab74b6af6f7b7bb58a1f36049bf22bb31e
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-02-09 16:26:50 -0800

    fetch profilename on transmitter ready

--- javascripts/transmit_text.js
+++ javascripts/transmit_text.js
@@ -17,12 +17,12 @@ var TextTransmitter = (function() {
     };
 
     function onTransmitterReady() {
+        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
         var transmit = Transmitter.transmitter(profilename);
         btn.addEventListener('click', onClick, false);
     };
 
     function onDOMLoad() {
-        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
         btn = document.querySelector('[data-quiet-send-button]');
         Transmitter.setReadyCallback(onTransmitterReady);
     };
