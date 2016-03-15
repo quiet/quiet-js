9f75877aab562f9d1b3dd7aecad03e8cfe8e038e
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-03-15 12:31:55 -0700

    visualizer

--- javascripts/lab.js
+++ javascripts/lab.js
@@ -1,8 +1,22 @@
 var QuietLab = (function() {
     var canvas;
+    var canvasCtx;
     var audioCtx;
     var analyser;
     var source;
+    var drawVisual;
+    var fftBuffer;
+
+    function drawFFT() {
+        drawVisual = requestAnimationFrame(drawFFT);
+        analyser.getFloatFrequencyDomainData(fftBuffer);
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < analyser.frequencyBinCount; i++) {
+            var magnitude = fftBuffer[i] * 4000;
+            canvasCtx.fillRect(i * 4, canvas.height, 3, -magnitude);
+        }
+    };
+
 
     function onGUMFail() {
         console.log("failed to create media stream source");
@@ -52,8 +66,12 @@ var QuietLab = (function() {
 
     function onDOMLoad() {
         canvas = document.querySelector("[data-quiet-lab-canvas]");
+        canvasCtx = canvas.getContext('2d');
         audioCtx = new (window.AudioContext || window.webkitAudioContext)();
         analyser = audioCtx.createAnalyser();
+        analyser.fftSize = 2048;
+        fftBuffer = new Float32Array(analyser.frequencyBinCount);
+
         var gUM = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
         gUM.call(navigator, gUMConstraints(), onGUM, onGUMFail);
     };
