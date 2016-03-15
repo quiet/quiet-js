1a13e79867d715100902d0fde2e4e3659655f498
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-03-15 11:59:36 -0700

    update links

--- javascripts/quiet-init.js
+++ javascripts/quiet-init.js
@@ -4,7 +4,7 @@ var QuietInitializer = (function() {
     Quiet.setLibfecPrefix("javascripts/");
 
     function onDOMLoad() {
-        var host = "brian-armstrong.github.io";
+        var host = "quiet.github.io";
         if ((host == window.location.host) && (window.location.protocol != "https:"))
             window.location.protocol = "https";
     };
