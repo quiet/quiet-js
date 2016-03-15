6dfa2eba2a061eba30f68c4c45c526e4e8f1f815
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2016-03-15 12:18:54 -0700

    initial lab

---
+++ javascripts/lab.js
@@ -0,0 +1,58 @@
+var QuietLab = (function() {
+    var canvas;
+    var audioCtx;
+    var analyser;
+    var source;
+
+    function onGUM(stream) {
+        source = audioCtx.createMediaStreamSource(stream);
+        source.connect(analyser);
+    };
+
+    function gUMConstraints() {
+        if (navigator.webkitGetUserMedia !== undefined) {
+            return {
+                audio: {
+                    optional: [
+                      {googAutoGainControl: false},
+                      {googAutoGainControl2: false},
+                      {echoCancellation: false},
+                      {googEchoCancellation: false},
+                      {googEchoCancellation2: false},
+                      {googDAEchoCancellation: false},
+                      {googNoiseSuppression: false},
+                      {googNoiseSuppression2: false},
+                      {googHighpassFilter: false},
+                      {googTypingNoiseDetection: false},
+                      {googAudioMirroring: false}
+                    ]
+                }
+            };
+        }
+        if (navigator.mozGetUserMedia !== undefined) {
+            return {
+                audio: {
+                    echoCancellation: false,
+                    mozAutoGainControl: false,
+                    mozNoiseSuppression: false
+                }
+            };
+
+        }
+        return {
+            audio: {
+                echoCancellation: false
+            }
+        };
+    };
+
+    function onDOMLoad() {
+        canvas = document.querySelector("[data-quiet-lab-canvas]");
+        audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = audioCtx.createAnalyser();
+        var gUM = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+        gUM.call(navigator, gUMConstraints(), onGUM);
+    };
+
+    document.addEventListener("DOMContentLoaded", onDOMLoad);
+})();
