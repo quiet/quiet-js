3c74c9d9f7f779f764aa037b87a43c16073eb2a8
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2018-01-20 15:21:57 -0800

    ook test

---
+++ test/receivetest.js
@@ -0,0 +1,49 @@
+var TextReceiver = (function() {
+    Quiet.init({
+        profilesPrefix: "/test/",
+        memoryInitializerPrefix: "/javascripts/",
+        libfecPrefix: "/javascripts/"
+    });
+    var target;
+    var content = new ArrayBuffer(0);
+    var warningbox;
+
+    var bytesReceived = 0;
+
+    function onReceive(recvPayload) {
+        bytesReceived += recvPayload.byteLength;
+        target.textContent = bytesReceived
+    };
+
+    function onReceiverCreateFail(reason) {
+        console.log("failed to create quiet receiver: " + reason);
+        warningbox.classList.remove("hidden");
+        warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
+    };
+
+    function onReceiveFail(num_fails) {
+    };
+
+    function onQuietReady() {
+        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
+        Quiet.receiver({profile: profilename,
+             onReceive: onReceive,
+             onCreateFail: onReceiverCreateFail,
+             onReceiveFail: onReceiveFail
+        });
+    };
+
+    function onQuietFail(reason) {
+        console.log("quiet failed to initialize: " + reason);
+        warningbox.classList.remove("hidden");
+        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
+    };
+
+    function onDOMLoad() {
+        target = document.querySelector('[data-quiet-receive-text-target]');
+        warningbox = document.querySelector('[data-quiet-warning]');
+        Quiet.addReadyCallback(onQuietReady, onQuietFail);
+    };
+
+    document.addEventListener("DOMContentLoaded", onDOMLoad);
+})();
---
+++ test/sendtest.js
@@ -0,0 +1,76 @@
+var TextTransmitter = (function() {
+    Quiet.init({
+        profilesPrefix: "/test/",
+        memoryInitializerPrefix: "/javascripts/",
+        libfecPrefix: "/javascripts/"
+    });
+    var btn;
+    var warningbox;
+    var transmit;
+
+    function calcTxQueueFrames() {
+        var totalQueueSize = 1 << 16;
+        return Math.ceil(totalQueueSize/(transmitter.frameLength + 4));
+    };
+
+    function buildFrame() {
+        var frame = new ArrayBuffer(transmitter.frameLength);
+        var byteView = new Uint8Array(frame);
+        for (var i = 4; i < byteView.length; i++) {
+            byteView[i] = Math.floor(Math.random() * 256);
+        }
+
+        return frame;
+    };
+
+    function initTxQueue() {
+        var numFrames = calcTxQueueFrames();
+        var frames = new ArrayBuffer(0);
+        for (var i = 0; i < numFrames; i++) {
+            frames = Quiet.mergeab(frames, buildFrame());
+        }
+        transmitter.transmit(frames);
+    };
+
+    function onClick(e) {
+        e.target.removeEventListener(e.type, arguments.callee);
+        e.target.disabled = true;
+        var originalText = e.target.innerText;
+        e.target.innerText = e.target.getAttribute('data-quiet-sending-text');
+        e.target.setAttribute('data-quiet-sending-text', originalText);
+        initTxQueue();
+    };
+
+    function onTransmitEnqueue() {
+        window.setTimeout(function() {
+            if (transmitter !== undefined) {
+                transmitter.transmit(buildFrame());
+            }
+        }, 0);
+    };
+
+    function onQuietReady() {
+        var profilename = document.querySelector('[data-quiet-profile-name]').getAttribute('data-quiet-profile-name');
+        transmit = Quiet.transmitter({
+            profile: profilename,
+            clampFrame: false,
+            onEnqueue: onTransmitEnqueue,
+        });
+        btn.addEventListener('click', onClick, false);
+    };
+
+    function onQuietFail(reason) {
+        console.log("quiet failed to initialize: " + reason);
+        warningbox.classList.remove("hidden");
+        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
+    };
+
+    function onDOMLoad() {
+        btn = document.querySelector('[data-quiet-send-button]');
+        textbox = document.querySelector('[data-quiet-text-input]');
+        warningbox = document.querySelector('[data-quiet-warning]');
+        Quiet.addReadyCallback(onQuietReady, onQuietFail);
+    };
+
+    document.addEventListener("DOMContentLoaded", onDOMLoad);
+})();
