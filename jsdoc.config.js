15cf7c0d5787f3ea2380c655314fb97ff90309d4
Leonard Kim <leonard.kim@8x8.com>
2019-01-22 18:49:35 -0800

    add jsdoc generator (i broke docs though)

---
+++ jsdoc.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    opts: {
+        destination: './dist/jsdoc',
+        recurse: true
+    },
+    plugins: [],
+    source: {
+        include: [ 'src' ],
+        includePattern: '.+\\.js$'
+    },
+    sourceType: 'module',
+    tags: {
+        allowUnknownTags: true,
+        dictionaries: [
+            'jsdoc',
+            'closure'
+        ]
+    },
+    templates: {
+        cleverLinks: false,
+        monospaceLinks: false
+    }
+};
