ca13e70f7727e96c29c8e644650e4d6ac0937ed2
Leonard Kim <leonard.kim@8x8.com>
2019-01-27 10:04:13 -0800

    adjust comment for encoder/decoder creator helper

--- src/libQuiet/createTranslator.js
+++ src/libQuiet/createTranslator.js
@@ -1,5 +1,16 @@
 import libQuietProvider from './libQuietProvider';
 
+/**
+ * Factory function for creating encoders and decoders
+ *
+ * @param {string} type - Whether this should construct and 'encoder' or
+ * 'decoder'.
+ * @param {Object} profile - The sound profile to use for encoding and decoding.
+ * @param {number} sampleRate - libquiet internally works at 44.1kHz but the
+ * local sound card may be a different rate. Inform quiet about our local sound
+ * card's sample rate so that it can resample to its internal sample rate.
+ * @returns {Encoder|Decoder}
+ */
 export default function createTranslator(type, profile, sampleRate) {
     const libQuiet = libQuietProvider.get();
 
@@ -14,9 +25,6 @@ export default function createTranslator(type, profile, sampleRate) {
         [ allProfiles, selectedProfileKey ]
     );
 
-    // libquiet internally works at 44.1kHz but the local sound card may be
-    // a different rate. Inform quiet about our local sound card's sample
-    // rate so that it can resample to its internal sample rate.
     const translator = libQuiet.ccall(
         `quiet_${type}_create`,
         'pointer',
